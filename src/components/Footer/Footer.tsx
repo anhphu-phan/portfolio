4ce0a9cf2d6188109221e94608eee1d743e929bc
anhphuphan <anhphuphan.cs@gmail.com>
2023-07-28 16:59:15 +0700

    update responsive for footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,16 +5,16 @@ import { rufina } from "@/fonts";
 
 export default function Footer() {
   return (
-    <div className="mt-auto flex gap-12 bg-slate-300 px-20 py-12">
+    <div className="mt-auto flex flex-col sm:flex-row gap-12 bg-slate-300 px-20 py-12">
       <div
         className={cn(
-          "flex grow-[3] items-center justify-center text-[length:calc(2rem+0.50625vw)] font-semibold",
+          "flex items-center grow justify-center text-[length:calc(2rem+0.50625vw)] font-semibold text-center",
           rufina.className,
         )}
       >
         Have a message for me?
       </div>
-      <div className="max-w-xl grow-[2]" id="contact">
+      <div className="w-full max-w-[36rem]" id="contact">
         <ContactForm />
       </div>
     </div>
