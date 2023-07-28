0e38362b87b733cfa4a4ca338b7c540f66bfba1d
anhphuphan <anhphuphan.cs@gmail.com>
2023-07-28 22:17:11 +0700

    remove some utils

--- src/utils/getProjectIds.ts
+++
@@ -1,9 +0,0 @@
-import * as path from "path";
-import  getFileNames  from "@/utils/getFileNames";
-
-export default async function getProjectIds(): Promise<number[]> {
-  const projectsDir = path.join(process.cwd(), "public/images/projects");
-  const dirNames = await getFileNames(projectsDir);
-
-  return dirNames.map(Number);
-}
