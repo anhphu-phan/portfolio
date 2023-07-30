fea2bbf3c453205c355dd840c59088414370b829
anhphuphan <anhphuphan.cs@gmail.com>
2023-07-30 19:01:18 +0700

    update project description field, change to markdown format

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -4,13 +4,16 @@ const projects: Project[] = [
   {
     name: "Akidex",
     summary:
-      "A project to help people to look up for anime, manga and visual novel, including their other information.",
-    description:
-      "A project to help people to look up for anime, manga and visual novel, including their other information.",
+      "A website to help people to look up for relative information about anime/manga/visual novel.",
+    description: `A website to help people to look up for relative information about anime/manga/visual novel.  
+      The website is built with React, Vite, MUI, GraphQL, TypeScript, NodeJS, Express, MongoDB.  
+      It uses [AniList](https://anilist.co)'s GraphQL API to fetch data for anime/manga and uses [VNDB](https://vndb.org)'s TCP-based API (pain) to fetch data for visual novel.
+      
+      `,
     techs: ["React", "Vite", "MUI", "GraphQL", "TypeScript", "NodeJS", "Express", "MongoDB"],
-    githubUrl: "",
+    githubUrl: "https://github.com/anhphu-phan/akidex",
     imageDir: "akidex",
-    liveUrl: "",
+    liveUrl: "https://phuphan.vercel.app",
   },
 ];
 
